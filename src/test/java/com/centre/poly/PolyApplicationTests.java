f231488cf73ea7dbee0e642e3f4e281e71c293a9
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2023-12-17 19:58:42 +0100

    first commit

---
+++ src/test/java/com/centre/poly/PolyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centre.poly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
