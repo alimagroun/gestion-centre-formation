b79c4463eec0cea2cf96c0a964dd6417856459b8
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-07-02 12:56:43 +0100

    Inject the jwt token using an HTTP interceptor

--- centre-ui/src/app/app.config.ts
+++ centre-ui/src/app/app.config.ts
@@ -3,8 +3,19 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
-import {provideHttpClient} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {HttpTokenInterceptor} from "./services/interceptor/http-token.service";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideClientHydration(), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:HttpTokenInterceptor,
+      multi:true
+    }
+  ]
 };
