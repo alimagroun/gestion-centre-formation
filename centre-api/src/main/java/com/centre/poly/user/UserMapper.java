6a49054b28ee5e0e4ff956c883b27c2d6ae629c8
Ahmed tiba <ahmed.tiba.1993@gmail.com>
2024-09-21 00:25:15 +0100

    Implements user management

--- centre-api/src/main/java/com/centre/poly/user/UserMapper.java
+++ centre-api/src/main/java/com/centre/poly/user/UserMapper.java
@@ -13,6 +13,7 @@ public class UserMapper {
     public UserResponse toResponse(User user) {
         return UserResponse.builder()
                 .id(user.getId())
+                .userName(user.getUsername())
                 .createdDate(user.getCreatedDate())
                 .person(toPerson(user.getPerson()))
                 .roles(user.getRoles())
