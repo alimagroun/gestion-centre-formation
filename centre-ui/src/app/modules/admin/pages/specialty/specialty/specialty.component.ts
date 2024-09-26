db28558c5c4102411fa2a615e75eb1b0fce83ca2
Ahmed tiba <ahmed.tiba.1993@gmail.com>
2024-09-26 16:17:16 +0100

    Implement classes module

--- centre-ui/src/app/modules/admin/pages/specialty/specialty/specialty.component.ts
+++ centre-ui/src/app/modules/admin/pages/specialty/specialty/specialty.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import {
+  PageResponseClasseFormationResponse
+} from "../../../../../services/models/page-response-classe-formation-response";
+import {ClasseFormationControllerService} from "../../../../../services/services/classe-formation-controller.service";
+import {NgForOf, NgIf} from "@angular/common";
+import {NgxPaginationModule} from "ngx-pagination";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-specialty',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    NgxPaginationModule,
+    RouterLink
+  ],
   templateUrl: './specialty.component.html',
   styleUrl: './specialty.component.scss'
 })
 export class SpecialtyComponent {
 
+  classeResponse: PageResponseClasseFormationResponse = {}
+  page: number = 0;
+  size: number = 10;
+  loading: boolean = false;
+
+  constructor(
+    private classeService: ClasseFormationControllerService
+  ) {
+  }
+
+  findAllClasse() {
+    this.loading = true;
+    this.classeService.findAllClasses({
+      page: this.page,
+      size: this.size
+    }).subscribe(res => {
+      this.classeResponse = res
+      this.loading = false
+    })
+  }
+
+  ngOnInit(): void {
+    this.findAllClasse()
+  }
+
+  loadPage(event: number) {
+    this.page = event - 1;
+    this.findAllClasse();
+  }
+
+  changePageSize(event: any) {
+    this.size = parseInt(event.target.value, 10);
+    this.findAllClasse();
+  }
 }
